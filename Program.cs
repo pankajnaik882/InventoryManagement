db791e8b0d6a7c2bad04f9e52abfa7b6e8d19205
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 19:57:12 +0530

    Add : Added Program File

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@
 
             InventoryDetails data = fetchInventory.Read(path);
 
+            Console.WriteLine("Type of Rice");
+            Console.WriteLine("*********************************************");
 
             for (int i = 0; i < data.typeofRice.Count; i++)
             {
@@ -16,7 +18,37 @@
                 Console.WriteLine(data.typeofRice[i].weight);
                 Console.WriteLine(data.typeofRice[i].price);
 
+                Console.WriteLine("************************************************");
+
             }
+
+
+            Console.WriteLine("Type of Pulses");
+            Console.WriteLine("*********************************************");
+
+            for (int i = 0; i < data.typeofPulses.Count; i++)
+            {
+                Console.WriteLine(data.typeofPulses[i].name);
+                Console.WriteLine(data.typeofPulses[i].weight);
+                Console.WriteLine(data.typeofPulses[i].price);
+
+                Console.WriteLine("************************************************");
+
+            }
+
+            Console.WriteLine("Type of Wheat");
+            Console.WriteLine("*********************************************");
+
+            for (int i = 0; i < data.typeofWheat.Count; i++)
+            {
+                Console.WriteLine(data.typeofWheat[i].name);
+                Console.WriteLine(data.typeofWheat[i].weight);
+                Console.WriteLine(data.typeofWheat[i].price);
+
+                Console.WriteLine("************************************************");
+
+            }
+
         }
     }
 }
