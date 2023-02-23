884aa4d505b4cb17a7141464681f6e98917c4a03
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 19:48:21 +0530

    Add : Added Program File

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace InventoryManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "C:\\Users\\USER\\source\\repos\\InventoryManagement\\InventoryManagement\\Inventory.json";
+            Fetchinventory fetchInventory = new Fetchinventory();
+
+            InventoryDetails data = fetchInventory.Read(path);
+
+
+            Console.WriteLine(data.typeofRice.name);
+            Console.WriteLine(data.typeofRice.weight);
+            Console.WriteLine(data.typeofRice.price);
+
+        }
+    }
+}
