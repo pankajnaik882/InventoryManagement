4d5c0acd50c9685fcb9dce00a2e84d412fa9fbbc
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 19:50:50 +0530

    Add : Getting Data from Inventory

---
+++ InventoryDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    {
+        public TypeofRice typeofRice;
+    }
+    public class TypeofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
