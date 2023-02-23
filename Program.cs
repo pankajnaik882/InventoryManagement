edbc1e271651cd4e88f2352785dd09641064ff20
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 19:59:31 +0530

    Add : Added Program File

--- Program.cs
+++ Program.cs
@@ -17,6 +17,9 @@
                 Console.WriteLine(data.typeofRice[i].name);
                 Console.WriteLine(data.typeofRice[i].weight);
                 Console.WriteLine(data.typeofRice[i].price);
+                int price = data.typeofRice[i].weight * data.typeofRice[i].price;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                data.typeofRice[i].name, data.typeofRice[i].weight, price);
 
                 Console.WriteLine("************************************************");
 
@@ -31,6 +34,10 @@
                 Console.WriteLine(data.typeofPulses[i].name);
                 Console.WriteLine(data.typeofPulses[i].weight);
                 Console.WriteLine(data.typeofPulses[i].price);
+                int price = data.typeofPulses[i].weight * data.typeofPulses[i].price;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                data.typeofPulses[i].name, data.typeofPulses[i].weight, price);
+
 
                 Console.WriteLine("************************************************");
 
@@ -44,11 +51,13 @@
                 Console.WriteLine(data.typeofWheat[i].name);
                 Console.WriteLine(data.typeofWheat[i].weight);
                 Console.WriteLine(data.typeofWheat[i].price);
+                int price = data.typeofWheat[i].weight * data.typeofWheat[i].price;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                data.typeofWheat[i].name, data.typeofWheat[i].weight, price);
 
                 Console.WriteLine("************************************************");
 
             }
-
         }
     }
 }
